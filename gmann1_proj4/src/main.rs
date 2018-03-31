07eaf63a1978b5758686542b50460d4f0f44d6db
Unknown <gregoryjmann26@gmail.com>
2018-03-30 22:21:51 -0400

    Started on project 4

---
+++ gmann1_proj4/src/main.rs
@@ -0,0 +1,23 @@
+// TODO: Read in nodes
+// TODO: Create the edges
+// TODO: Create the table (Use Dijkstra’s algorithm to find the shortest cost and path between nodes.)
+// TODO: Command line options
+// TODO: Create Readme.txt
+
+pub static INPUT_FILE: &str = "gmann1_input.txt";
+pub static OUPUT_FILE: &str = "gmann1_output.txt";
+
+struct Node {
+    name: String,
+    // TODO make an array / list of edges for this
+    // edges:
+}
+
+struct Graph {
+    // TODO: Make an array / list of nodes
+    nodes: Node,
+}
+
+fn main() {
+    println!("{}", INPUT_FILE);
+}
